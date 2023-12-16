f6b7b181ef8e66701f58faf0d805115a09d05acd
aakashbishnoi1 <8xaakash@gmail.com>
2023-12-16 17:04:35 +0530

    Opening the file
    and handling any errors

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use std::{fs::File, io};
+use std::{fs::File, io::{self, Read}};
+use io::ErrorKind;
 fn main() {
     println!("Enter the file name");
     let mut filename = String::new();
     io::stdin()
         .read_line(&mut filename)
         .expect("Couldn't read the input");
-    
-    
+    let filename = filename.trim();
+
+    let target_file = File::open(filename);
+    let target_file = match target_file {
+        Ok(file) => file,
+        Err(error) => match error.kind()  {
+                ErrorKind::NotFound => panic!("Error: {}", error),
+                other_error => panic!("Problem opening the file {}", other_error),
+
+        },
+    };
+
 }
